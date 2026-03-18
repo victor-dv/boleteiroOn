2cbf43062e4eaf3a03a53d25b7f44a844251b820
victor <js813417@gmail.com>
2026-03-18 08:35:58 -0300

    Initial commit

---
+++ src/main/java/br/com/boleiroOn/BoleiroOnApplication.java
@@ -0,0 +1,13 @@
+package br.com.boleiroOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoleiroOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoleiroOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/boleiroOn/BoleiroOnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boleiroOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoleiroOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
