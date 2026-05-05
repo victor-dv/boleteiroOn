3f789ec9efad21b0e8bcf1f968753b811de381ed
victor-dv <js813417@gmail.com>
2026-05-05 16:11:21 -0300

    git fit - Finalizando utlimas atualizações

--- src/main/java/br/com/boleiroOn/BoleiroOnApplication.java
+++ src/main/java/br/com/boleiroOn/BoleiroOnApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableCaching
 public class BoleiroOnApplication {
 
 	public static void main(String[] args) {
